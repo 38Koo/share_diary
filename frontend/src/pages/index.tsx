def15a829a8e1d20d61b9548aadeb567a363f9a3
宮本航平 <38koo@miyamotheinoAir.AirPort>
2023-10-30 23:03:06 +0900

    黒背景ブラウザでも操作できるように背景色を追加

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Calendar } from '../component/feature/Calendar'
 
 export default function Home() {
   return (
-    <div className='flex justify-center py-3'>
+    <div className='flex justify-center bg-[#FCF5E8] py-3'>
       <Calendar />
     </div>
   )
